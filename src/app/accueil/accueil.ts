f761fd6791dc713936b66e603218ca7676437797
Florian <florian.souquere@gmail.com>
2026-04-03 10:23:15 +0200

    Ajout de la page accueil avec Bootstrap et formulaire Angular

--- src/app/accueil/accueil.ts
+++ src/app/accueil/accueil.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-accueil',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './accueil.html',
   styleUrl: './accueil.css',
 })
 export class Accueil {
+  public monTitre: string;
+  public monIntroduction: string;
+  public maPhraseAccroche: string;
 
-public monTitre: string;
-public monIntroduction: string;
-public maPhraseAccroche: string;
+  public champSaisi = new FormControl('');
 
   constructor() {
-    this.monTitre = 'Ma commune';
+    this.monTitre = 'Ma Commune';
     this.monIntroduction = 'Informations sur les communes';
     this.maPhraseAccroche = 'Insérez le code du département';
   }
 
+  public afficherChampSaisi(): void {
+    alert(this.champSaisi.value);
+  }
 }
