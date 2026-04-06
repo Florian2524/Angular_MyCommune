4671cf497ab1185dae45522dcbef277d20247d1e
Florian <florian.souquere@gmail.com>
2026-04-06 11:55:48 +0200

    Ajout de la récupération des communes et affichage des informations sélectionnées

--- src/app/accueil/accueil.ts
+++ src/app/accueil/accueil.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-accueil',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './accueil.html',
   styleUrl: './accueil.css',
 })
@@ -13,14 +15,42 @@ export class Accueil {
   public maPhraseAccroche: string;
 
   public champSaisi = new FormControl('');
+  public communeSelectionnee = new FormControl('');
+  public listeCommunes: any[] = [];
+  public communeAffichee: any = null;
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.monTitre = 'Ma Commune';
     this.monIntroduction = 'Informations sur les communes';
     this.maPhraseAccroche = 'Insérez le code du département';
   }
 
   public afficherChampSaisi(): void {
-    alert(this.champSaisi.value);
+    const codeDepartement = this.champSaisi.value;
+
+    this.http
+      .get<any[]>(`https://geo.api.gouv.fr/departements/${codeDepartement}/communes`)
+      .subscribe((donnees) => {
+        this.listeCommunes = donnees;
+        this.communeSelectionnee.setValue('');
+        this.communeAffichee = null;
+        console.log(this.listeCommunes);
+      });
+  }
+
+  public validerCommune(): void {
+    const nomCommune = this.communeSelectionnee.value;
+
+    const communeTrouvee = this.listeCommunes.find(
+      (commune) => commune.nom === nomCommune
+    );
+
+    if (communeTrouvee) {
+      this.communeAffichee = {
+        nom: communeTrouvee.nom,
+        codePostal: communeTrouvee.codesPostaux?.[0] || 'Non renseigné',
+        population: communeTrouvee.population,
+      };
+    }
   }
 }
